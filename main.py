6c9761e7afb4303997c65db899e4d584dad7e7a5
lynnlangit <lynnlangit@live.com>
2015-05-24 00:34:15 -0700

    updated index page

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import google.appengine.ext.webapp.template as template
 import os
 import cloudstorage as gcs
 import logging
+
+
 class HelloWebapp2(webapp2.RequestHandler):
     def get(self):
         self.response.content_type = 'text/html'
@@ -12,18 +14,6 @@ class HelloWebapp2(webapp2.RequestHandler):
         self.response.write(file.read())
         file.close()
 
-    # def get(self):
-    #     bucket_name = 'virtualproctor'
-    #     bucket = '/' + bucket_name
-    #     try:
-    #         stats = gcs.listbucket(bucket, max_keys=10)
-    #         names = [x.filename.replace('/virtualproctor/','') for x in stats]
-    #
-    #     except Exception as e:
-    #         logging.exception(e)
-    #     path = os.path.join(os.path.dirname(__file__), 'index.html')
-    #     template_values = {'names': names }
-    #     self.response.write(template.render(path,template_values))
 
 app = webapp2.WSGIApplication([
     ('/', HelloWebapp2),
